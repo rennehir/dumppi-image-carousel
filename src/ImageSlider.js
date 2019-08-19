2b3f20c245aaa737c3f34c8b657ff2b77b14841f
rennehir <git@rennehir.fi>
2019-08-19 21:18:40 +0300

    unionfind is no longer being molested.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -22,43 +22,46 @@ const GET_IMAGES = gql`
 const ImageSlider = () => {
   const { error, loading, data } = useQuery(GET_IMAGES, { pollInterval: 2 * 60 * 1000 });
 
-  if (error || loading || !data) return null;
+  if (error || loading || !data || !data.photoSlider) return null;
 
   const {
     photoSlider: { intervalTime, photos },
   } = data;
 
-  const settings = {
-    showArrows: false,
-    showStatus: false,
-    showIndicators: false,
-    showThumbs: false,
-    infiniteLoop: true,
-    autoPlay: true,
-    transitionTime: 500,
-    interval: intervalTime * 1000,
-  };
+  if (photos.length > 0 && intervalTime) {
+    const settings = {
+      showArrows: false,
+      showStatus: false,
+      showIndicators: false,
+      showThumbs: false,
+      infiniteLoop: true,
+      autoPlay: true,
+      stopOnHover: false,
+      interval: intervalTime * 1000,
+      transitionTime: 500,
+    };
 
-  return (
-    <Carousel {...settings}>
-      {photos.map(photo => {
-        const { id, image } = photo;
-        return (
-          <div
-            key={id}
-            style={{
-              width: '100vw',
-              height: '100vh',
-              backgroundImage: `url(${image.url})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'contain',
-              backgroundPosition: 'center',
-            }}
-          />
-        );
-      })}
-    </Carousel>
-  );
+    return (
+      <Carousel {...settings}>
+        {photos.map(photo => {
+          const { id, image } = photo;
+          return (
+            <div
+              key={id}
+              style={{
+                width: '100vw',
+                height: '100vh',
+                backgroundImage: `url(${image.url})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'contain',
+                backgroundPosition: 'center',
+              }}
+            />
+          );
+        })}
+      </Carousel>
+    );
+  }
 };
 
 export default ImageSlider;
